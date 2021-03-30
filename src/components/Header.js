e0ec8b3eadc196c1c09f94ae170896b8bde5805e
Ben Domschot <Bdomschot@gmail.com>
2021-03-30 10:22:01 -0700

    Added Bootstrap, created Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+
+function Header(){
+
+    <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Google Book Search</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/Search">Search</Nav.Link>
+      <Nav.Link href="/Saved">Saved</Nav.Link>
+     </Nav>
+    </Navbar>
+}
