13ceaa6771cf99f9f29e9e8446101a10cbf220c6
Ben Domschot <Bdomschot@gmail.com>
2021-04-03 11:35:41 -0700

    forgot some routes, added.

--- routes/api/books.js
+++ routes/api/books.js
@@ -4,6 +4,11 @@ const bookControllers = require('../../controllers/bookControllers');
 router.route('/')
     
     .post(bookControllers.saveBook)
+    .get(bookControllers.displayBooks);
+
+router.route('/:id')
+
+    .delete(bookControllers.deleteBook);
 
 
 module.exports = router
